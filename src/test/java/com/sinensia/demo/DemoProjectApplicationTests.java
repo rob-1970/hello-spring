53a7763d19cd773dfbf2f1e3a751948361d9accd
rob-1970 <rob.unic.1970@gmail.com>
2022-01-31 13:21:24 +0100

    ADDED appCanAddNull => Controlar NULL EXCEPTION

--- src/test/java/com/sinensia/demo/DemoProjectApplicationTests.java
+++ src/test/java/com/sinensia/demo/DemoProjectApplicationTests.java
@@ -1,26 +1,33 @@
 package com.sinensia.demo;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
 class DemoProjectApplicationTests {
 
+	@Autowired TestRestTemplate restTemplate;
+
 	@Test /* "Error en carga de contexto " */
 	void contextLoads() {
 	}
 
-	@Test
-	void manualError() throws Exception {
-		throw new Exception("Error generado manualmente en carga de contexto ");
-	}
+	//@Test
+	//void manualError() throws Exception {
+	//	throw new Exception("Error generado manualmente en carga de contexto ");
+	//}
 
 //	@Test /* REALIZAR una prueba en navegador sobre la url RAIZ para cualquier puerto */
 //	void rootTest (@Autowired WebTestClient webClient) { /* Autowired INDEXACIO o ANOTACIO MAGICA de SPRING */
@@ -64,5 +71,41 @@ class DemoProjectApplicationTests {
 		assertThat(restTemplate.getForObject("/add?a=3.45&b=3.55", String.class)).isEqualTo("7.0");
 	}
 
+	@ParameterizedTest
+	@CsvSource({
+			"a, Hello a!",
+			"b, Hello b!",
+			//"first%20last, Hello first last!"
+	})
+	void helloParamNamesCsv(String name, String expected) {
+		assertThat(restTemplate.getForObject("/hello?name="+name, String.class)).isEqualTo(expected);
+	}
+
+	@Nested
+	@DisplayName(value="Application tests")
+	class AppTests {
+
+		@Autowired
+		private DemoProjectApplication app;
+
+		@Test
+		void appCanAddReturnsInteger() {
+			assertThat(app.canAdd(1.0, 2.0)).isEqualTo(3);
+		}
+
+		@Test
+		void appCanAddReturnsFloat() {
+			assertThat(app.canAdd(1.5, 2.0)).isEqualTo(3.5f);
+		}
+
+		@Test
+		void appCanAddNull() {
+			Exception thrown = assertThrows(NullPointerException.class,() -> {
+				Double ret = app.canAdd((Double) null, 2.0);
+			});
+			assertTrue(thrown.toString().contains("NullPointerException"));
+			//thrown.getMessage().contains("");              alternativamente comprovar que contiene ""
+		}
+	}
 }
 
